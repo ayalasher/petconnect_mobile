7110fa6c80d86829afb522aa09689a1898771d56
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-04-03 17:50:18 +0300

    added user and Spp screens

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pet connect backend mobile</Text>
+      <Text>Pet connect mobile</Text>
       <StatusBar style="auto" />
     </View>
   );
